a0e160a0848e1295c0281ac6c964730103c9f83d
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-03-25 12:48:43 -0500

    workin at home

--- Chapter 08/js/example.js
+++ Chapter 08/js/example.js
@@ -27,26 +27,101 @@ window.onload = function(){
         .style("fill", "#FFFFFF"); //fill color
     
     //Example 2.5 line 1
-    var dataArray = [10, 20, 30, 40, 50];
+    var cityPop = [
+        { 
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
 
-    var circles = container.selectAll(".circles") //but wait--there are no circles yet!
-        .data(dataArray) //here we feed in an array
+
+    //above Example 2.8 line 20
+    //find the minimum value of the array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+
+    //find the maximum value of the array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+
+    //scale for circles center y coordinate
+    var y = d3.scaleLinear()
+        .range([440, 95])
+        .domain([
+            minPop,
+            maxPop
+        ]);
+    //color scale generator 
+    var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+        .domain([
+            minPop, 
+            maxPop
+        ]);
+
+    //above Example 2.8 line 20
+    var x = d3.scaleLinear() //create the scale
+        .range([90, 810]) //output min and max
+        .domain([0, 3]); //input min and max
+
+    //Example 2.6 line 3
+    var circles = container.selectAll(".circles") //create an empty selection
+        .data(cityPop) //here we feed in an array
         .enter() //one of the great mysteries of the universe
-        .append("circle") //add a circle for each datum
-        .attr("class", "circles") //apply a class name to all circles
-        .attr("r", function(d, i){ //circle radius
-            console.log("d:", d, "i:", i); //let's take a look at d and i
-            return d;
+        .append("circle") //inspect the HTML--holy crap, there's some circles there
+        .attr("class", "circles")
+        .attr("id", function(d){
+            return d.city;
         })
-        .attr("cx", function(d, i){ //x coordinate
-            return 70 + (i * 180);
+        .attr("r", function(d){
+            //calculate the radius based on population value as circle area
+            var area = d.population * 0.01;
+            return Math.sqrt(area/Math.PI);
         })
-        .attr("cy", function(d){ //y coordinate
-            return 450 - (d * 5);
-        });
+        //Example 2.8 line 34
+        .attr("cx", function(d, i){
+            //use the scale generator with the index to place each circle horizontally
+            return x(i);
+        })
+        //Example 2.8 line 38
+        .attr("cy", function(d){
+            return y(d.population);
+        })
+        .style("fill", function(d, i){ //add a fill based on the color scale generator
+            return color(d.population);
+        })
+        .style("stroke", "#000"); //black circle stroke
 
+    //Example 3.6 line 1...create y axis generator
+    var yAxis = d3.axisLeft(y);
 
+    //Example 3.8 line 1...create axis g element and add axis
+    var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(50, 0)")
+        .call(yAxis);
+
+    yAxis(axis);
 
     console.log(innerRect)
     console.log(container)
+    console.log(x)
+    console.log(y)
 };
