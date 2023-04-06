70ff2a171b15e9ac1fab43b620ea996b8f40d86e
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-04-06 17:28:46 -0500

    commit before reproject

--- Chapter 10/js/week10work.js
+++ Chapter 10/js/week10work.js
@@ -1,119 +1,127 @@
-//begin script when window loads
-window.onload = setMap();
+//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+(function () {
 
-function setMap(){
+    //pseudo-global variables
+    var attrArray = ["2023_Population", "vGDP_PrCap", "Area_Mi", "vPop_Dens", "Life_Exp"]; //list of attributes
+    var expressed = attrArray[0]; //initial attribute
 
-    //map frame dimensions
-    var width = 960,
-        height = 460;
+    //begin script when window loads
+    window.onload = setMap();
 
-    //create new svg container for the map
-    var map = d3.select("body")
-        .append("svg")
-        .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
+    function setMap() {
 
-    //create Albers equal area conic projection centered on France
-    var projection = d3.geoAlbers()
-        .center([0, 45])
-        .rotate([103, 0, 0])
-        .parallels([29.5, 45.5])
-        .scale(300)
-        .translate([width / 2, height / 2]);
-        
-    var path = d3.geoPath().projection(projection);
+        //map frame dimensions
+        var width = 960,
+            height = 750;
 
-    //use Promise.all to parallelize asynchronous data loading
-    var promises = [    
-        d3.csv("data/NAmericaData.csv"), //load attributes from csv
-        d3.json("data/WorldCountries.topojson"), //load basemap data       
-        d3.json("data/NAmerica.topojson"), //load choropleth spatial data
-    ];    
-    Promise.all(promises).then(callback);
+        //create new svg container for the map
+        var map = d3.select("body")
+            .append("svg")
+            .attr("class", "map")
+            .attr("width", width)
+            .attr("height", height);
 
-    function callback(data){
-        var csvData = data[0], world = data[1], namerica = data[2];
+        //create Albers equal area conic projection centered on France
+        var projection = d3.geoAlbers()
+            .center([0, 45])
+            .rotate([103, 0, 0])
+            .parallels([29.5, 45.5])
+            .scale(300)
+            .translate([width / 2, height / 2]);
 
-        //translate namerica TopoJSON
-        var worldCountries = topojson.feature(world, world.objects.WorldCountries),
-            namericaCountries = topojson.feature(namerica, namerica.objects.NAmerica).features;
-        console.log(csvData);
-        console.log(namericaCountries);
-            
-        //variables for data join
-        var attrArray = ["2023_Population", "vGDP_PrCap", "Area_Mi", "vPop_Dens", "Life_Exp"];
+        var path = d3.geoPath().projection(projection);
 
-        //loop through csv to assign each set of csv attribute values to geojson region
-        for (var i=0; i<csvData.length; i++){
-            var csvRegion = csvData[i]; //the current region
-            var csvKey = csvRegion.SOVEREIGNT; //the CSV primary key
+        //use Promise.all to parallelize asynchronous data loading
+        var promises = [
+            d3.csv("data/NAmericaData.csv"), //load attributes from csv
+            d3.json("data/WorldCountries.topojson"), //load basemap data       
+            d3.json("data/NAmerica.topojson"), //load choropleth spatial data
+        ];
+        Promise.all(promises).then(callback);
 
-            //loop through geojson regions to find correct region
-            for (var a=0; a<namericaCountries.length; a++){
+        function callback(data) {
+            var csvData = data[0], world = data[1], namerica = data[2];
 
-                var geojsonProps = namericaCountries[a].properties; //the current region geojson properties
-                var geojsonKey = geojsonProps.SOVEREIGNT; //the geojson primary key
+            //translate namerica TopoJSON
+            var worldCountries = topojson.feature(world, world.objects.WorldCountries),
+                namericaCountries = topojson.feature(namerica, namerica.objects.NAmerica).features;
+            console.log(csvData);
+            console.log(namericaCountries);
 
-                //where primary keys match, transfer csv data to geojson properties object
-                if (geojsonKey == csvKey){
+            //variables for data join
+            var attrArray = ["2023_Population", "vGDP_PrCap", "Area_Mi", "vPop_Dens", "Life_Exp"];
 
-                    //assign all attributes and values
-                    attrArray.forEach(function(attr){
-                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
-                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
-                    });
+            //loop through csv to assign each set of csv attribute values to geojson region
+            for (var i = 0; i < csvData.length; i++) {
+                var csvRegion = csvData[i]; //the current region
+                var csvKey = csvRegion.SOVEREIGNT; //the CSV primary key
+
+                //loop through geojson regions to find correct region
+                for (var a = 0; a < namericaCountries.length; a++) {
+
+                    var geojsonProps = namericaCountries[a].properties; //the current region geojson properties
+                    var geojsonKey = geojsonProps.SOVEREIGNT; //the geojson primary key
+
+                    //where primary keys match, transfer csv data to geojson properties object
+                    if (geojsonKey == csvKey) {
+
+                        //assign all attributes and values
+                        attrArray.forEach(function (attr) {
+                            var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                            geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                        });
+                    };
                 };
             };
+            console.log(namericaCountries)
+
+            //translate namerica TopoJSON
+            var worldCountries = topojson.feature(world, world.objects.WorldCountries),
+                namericaCountries = topojson.feature(namerica, namerica.objects.NAmerica).features;
+
+            //Example 2.6 line 1...create graticule generator
+            var graticule = d3.geoGraticule().step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+            //create graticule background
+            var gratBackground = map
+                .append("path")
+                .datum(graticule.outline()) //bind graticule background
+                .attr("class", "gratBackground") //assign class for styling
+                .attr("d", path) //project graticule
+
+            //Example 2.6 line 5...create graticule lines
+            var gratLines = map
+                .selectAll(".gratLines") //select graticule elements that will be created
+                .data(graticule.lines()) //bind graticule lines to each element to be created
+                .enter() //create an element for each datum
+                .append("path") //append each element to the svg as a path element
+                .attr("class", "gratLines") //assign class for styling
+                .attr("d", path); //project graticule lines
+
+            //translate namerica TopoJSON
+            var worldCountries = topojson.feature(world, world.objects.WorldCountries),
+                namericaCountries = topojson.feature(namerica, namerica.objects.NAmerica).features;
+
+            //add namerica countries to map
+            var world = map
+                .append("path")
+                .datum(worldCountries)
+                .attr("class", "world")
+                .attr("d", path);
+
+            //add namerica regions to map
+            var namerica = map
+                .selectAll(".namericaCountries")
+                .data(namericaCountries)
+                .enter()
+                .append("path")
+                .attr("class", function (d) {
+                    return "namericaCountries " + d.properties.SOVEREIGNT;
+                })
+                .attr("d", path);
+
+
+
         };
-        console.log(namericaCountries)
-
-        //translate namerica TopoJSON
-        var worldCountries = topojson.feature(world, world.objects.WorldCountries),
-            namericaCountries = topojson.feature(namerica, namerica.objects.NAmerica).features;        
-            
-        //Example 2.6 line 1...create graticule generator
-        var graticule = d3.geoGraticule().step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
-
-        //create graticule background
-        var gratBackground = map
-            .append("path")
-            .datum(graticule.outline()) //bind graticule background
-            .attr("class", "gratBackground") //assign class for styling
-            .attr("d", path) //project graticule
-
-        //Example 2.6 line 5...create graticule lines
-        var gratLines = map
-            .selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines
-
-        //translate namerica TopoJSON
-        var worldCountries = topojson.feature(world, world.objects.WorldCountries),
-            namericaCountries = topojson.feature(namerica, namerica.objects.NAmerica).features;
-        console.log(csvData);
-        console.log(namerica);
-
-        //add namerica countries to map
-        var countries = map
-            .append("path")
-            .datum(worldCountries)
-            .attr("class", "world")
-            .attr("d", path);
-
-        //add namerica regions to map
-        var regions = map
-            .selectAll(".namericaCountries")
-            .data(namericaCountries)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "namericaCountries " + d.properties.SOVEREIGNT;
-            })
-            .attr("d", path);
-            
     };
-};
+})(); //last line of main.js
